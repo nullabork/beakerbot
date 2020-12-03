3b1c5635651a60c03b2ee3db5280f2defe3979fb
fax <fax@fax.com>
2020-12-04 00:38:11 +1300

    whoop

--- bot/config/memes.js
+++ bot/config/memes.js
@@ -1,376 +1,460 @@
 let types = {
-  IMAGE: 'IMAGE',
-  TEXT: 'TEXT',
-  IMAGE_OR_TEXT: 'IMAGE_OR_TEXT',
-  PLOT: 'PLOT',
-  PARAM_TEXT: 'PARAM_TEXT',
-
-}
+  IMAGE: "IMAGE",
+  TEXT: "TEXT",
+  IMAGE_OR_TEXT: "IMAGE_OR_TEXT",
+  PLOT: "PLOT",
+  PARAM_TEXT: "PARAM_TEXT",
+};
 
 //https://www.useragentman.com/matrix/
 
 const memes = [
   {
-    keyword: 'beaker',
-    template: 'default.html',
-    
+    keyword: "beaker",
+    template: "default.html",
+
     meme: {
       w: 1920,
       h: 1200,
       image: {
-        url: 'images/beaker.jpg',
-        align: 'right center'
-      }
+        url: "images/beaker.jpg",
+        align: "right center",
+      },
     },
     defaultFrameIndex: 0,
-    
-    frames: [{
-      type: types.IMAGE_OR_TEXT,
-      x: 100, y: 100, w: 1000, h: 800, a: 0,
 
-      text: {
-        valign: 'center',
-        halign: 'flex-start',
-        value: ''
-      }
-    }]
+    frames: [
+      {
+        type: types.IMAGE_OR_TEXT,
+        x: 100,
+        y: 100,
+        w: 1000,
+        h: 800,
+        a: 0,
+
+        text: {
+          valign: "center",
+          halign: "flex-start",
+          value: "",
+        },
+      },
+    ],
   },
   {
-    keyword: 'oscar',
-    template: 'default.html',
-    
+    keyword: "oscar",
+    template: "default.html",
+
     meme: {
       w: 1920,
       h: 1080,
       image: {
-        url: 'images/oscar.jpg',
-        align: 'center center'
-      }
+        url: "images/oscar.jpg",
+        align: "center center",
+      },
     },
     defaultFrameIndex: 0,
-    frames: [{
-      type: types.IMAGE_OR_TEXT,
-      x: 100, y: 100, w: 900, h: 800, a: 0,
+    frames: [
+      {
+        type: types.IMAGE_OR_TEXT,
+        x: 100,
+        y: 100,
+        w: 900,
+        h: 800,
+        a: 0,
 
-      text: {
-        valign: 'center',
-        halign: 'flex-start',
-        value: ''
-      }
-    }]
+        text: {
+          valign: "center",
+          halign: "flex-start",
+          value: "",
+        },
+      },
+    ],
   },
   {
-    keyword: 'waldorf',
-    template: 'default.html',
-    
+    keyword: "waldorf",
+    template: "default.html",
+
     meme: {
       w: 900,
       h: 909,
       image: {
-        url: 'images/waldorf.jpg',
-        align: 'center center'
-      }
+        url: "images/waldorf.jpg",
+        align: "center center",
+      },
     },
     defaultFrameIndex: 0,
-    frames: [{
-      type: types.IMAGE_OR_TEXT,
-      x: 124, y: 477, w: 637, h: 419, a: 0,
+    frames: [
+      {
+        type: types.IMAGE_OR_TEXT,
+        x: 124,
+        y: 477,
+        w: 637,
+        h: 419,
+        a: 0,
 
-      text: {
-        valign: 'center',
-        halign: 'flex-start',
-        value: ''
-      }
-    }]
+        text: {
+          valign: "center",
+          halign: "flex-start",
+          value: "",
+        },
+      },
+    ],
   },
   {
-    keyword: 'grump',
-    template: 'default.html',
-    
+    keyword: "grump",
+    template: "default.html",
+
     meme: {
       w: 498,
       h: 331,
       image: {
-        url: 'images/grump.jpg',
-        align: 'center center'
-      }
+        url: "images/grump.jpg",
+        align: "center center",
+      },
     },
     defaultFrameIndex: 0,
-    frames: [{
-      type: types.IMAGE_OR_TEXT,
-      x: 242, y: 30, w: 218, h: 264, a: 0,
+    frames: [
+      {
+        type: types.IMAGE_OR_TEXT,
+        x: 242,
+        y: 30,
+        w: 218,
+        h: 264,
+        a: 0,
 
-      text: {
-        valign: 'center',
-        halign: 'flex-end',
-        color: 'black',
-        value: ''
-      }
-    }]
+        text: {
+          valign: "center",
+          halign: "flex-end",
+          color: "black",
+          value: "",
+        },
+      },
+    ],
   },
   {
-    keyword: 'cookie',
-    template: 'default.html',
-    
+    keyword: "cookie",
+    template: "default.html",
+
     meme: {
       w: 1016,
       h: 657,
       image: {
-        url: 'images/cookie.jpg',
-        align: 'center bottom'
-      }
+        url: "images/cookie.jpg",
+        align: "center bottom",
+      },
     },
     defaultFrameIndex: 0,
-    frames: [{
-      type: types.IMAGE_OR_TEXT,
-      x: 43,  y: 80, w: 288, h: 403, a: 0,
+    frames: [
+      {
+        type: types.IMAGE_OR_TEXT,
+        x: 43,
+        y: 80,
+        w: 288,
+        h: 403,
+        a: 0,
 
-      text: {
-        valign: 'center',
-        halign: 'center',
-        value: ''
-      }
-    }]
+        text: {
+          valign: "center",
+          halign: "center",
+          value: "",
+        },
+      },
+    ],
   },
   {
-    keyword: 'vegeta',
-    template: 'default.html',
-    
+    keyword: "vegeta",
+    template: "default.html",
+
     meme: {
       w: 1166,
       h: 1025,
       image: {
-        url: 'images/vege.png',
-        align: 'center center'
-      }
+        url: "images/vege.png",
+        align: "center center",
+      },
     },
     defaultFrameIndex: 0,
-    frames: [{
-      type: types.IMAGE_OR_TEXT,
-      x: 471.727,  y: 341.027, w: 371.814, h: 210.156, a: 0,
+    frames: [
+      {
+        type: types.IMAGE_OR_TEXT,
+        x: 471.727,
+        y: 341.027,
+        w: 371.814,
+        h: 210.156,
+        a: 0,
 
-      text: {
-        valign: 'center',
-        halign: 'center',
-        color: 'black',
-        shadow: 'white',
-        value: ''
-      }
-    }]
+        text: {
+          valign: "center",
+          halign: "center",
+          color: "black",
+          shadow: "white",
+          value: "",
+        },
+      },
+    ],
   },
   {
-    keyword: 'animal',
-    template: 'default.html',
-    
+    keyword: "animal",
+    template: "default.html",
+
     meme: {
       w: 1446,
       h: 1500,
       image: {
-        url: 'images/animal.jpg',
-        align: 'center right'
-      }
+        url: "images/animal.jpg",
+        align: "center right",
+      },
     },
     defaultFrameIndex: 0,
-    frames: [{
-      type: types.IMAGE_OR_TEXT,
-      x: 30.2, y: 482.9, w: 697.1, h: 514.3, a: 0,
+    frames: [
+      {
+        type: types.IMAGE_OR_TEXT,
+        x: 30.2,
+        y: 482.9,
+        w: 697.1,
+        h: 514.3,
+        a: 0,
 
-      text: {
-        valign: 'center',
-        halign: 'flex-end',
-        color: 'black',
-        shadow: 'white',
-        strokeSize: '2px',
-        strokeColor: 'rgba(255,255,255,0.3)',
-        value: ''
-      }
-    }]
-  }, {
-    keyword: 'swedish',
-    template: 'default.html',
-    
+        text: {
+          valign: "center",
+          halign: "flex-end",
+          color: "black",
+          shadow: "white",
+          strokeSize: "2px",
+          strokeColor: "rgba(255,255,255,0.3)",
+          value: "",
+        },
+      },
+    ],
+  },
+  {
+    keyword: "swedish",
+    template: "default.html",
+
     meme: {
       w: 1000,
       h: 525,
       image: {
-        url: 'images/swedish.jpg',
-        align: 'center center'
-      }
+        url: "images/swedish.jpg",
+        align: "center center",
+      },
     },
     defaultFrameIndex: 0,
-    frames: [{
-      type: types.IMAGE_OR_TEXT,
-      x: 15, y: 135, w: 293,  h: 306,  a: 0,
+    frames: [
+      {
+        type: types.IMAGE_OR_TEXT,
+        x: 15,
+        y: 135,
+        w: 293,
+        h: 306,
+        a: 0,
 
-      text: {
-        valign: 'center',
-        halign: 'flex-start',
-        value: ''
-      }
-    }]
+        text: {
+          valign: "center",
+          halign: "flex-start",
+          value: "",
+        },
+      },
+    ],
   },
   {
-    keyword: 'snoop',
-    template: 'default.html',
-    
+    keyword: "snoop",
+    template: "default.html",
+
     meme: {
-      w: 1384 ,
-      h: 821 ,
+      w: 1384,
+      h: 821,
       image: {
-        url: 'images/snoop.jpg',
-        align: 'center center'
-      }
+        url: "images/snoop.jpg",
+        align: "center center",
+      },
     },
     defaultFrameIndex: 0,
-    frames: [{
-      type: types.IMAGE_OR_TEXT,
-      x: 29, y: 29, w: 821, h: 763, a: 0,
-      text: {
-        valign: 'center',
-        halign: 'center',
-        color: 'black',
-        shadow: 'white',
-        strokeSize: '0px',
-        strokeColor: 'rgba(255,255,255,0.01)',
-        value: ''
-      }
-    }]
+    frames: [
+      {
+        type: types.IMAGE_OR_TEXT,
+        x: 29,
+        y: 29,
+        w: 821,
+        h: 763,
+        a: 0,
+        text: {
+          valign: "center",
+          halign: "center",
+          color: "black",
+          shadow: "white",
+          strokeSize: "0px",
+          strokeColor: "rgba(255,255,255,0.01)",
+          value: "",
+        },
+      },
+    ],
   },
   {
-    keyword: 'trumpdesk',
-    template: 'default.html',
-    
+    keyword: "trumpdesk",
+    template: "default.html",
+
     meme: {
-      w: 853 ,
-      h: 844 ,
+      w: 853,
+      h: 844,
       image: {
-        url: 'images/trump_background.png',
-        align: 'center center'
-      }
+        url: "images/trump_background.png",
+        align: "center center",
+      },
     },
     defaultFrameIndex: 0,
-    frames: [{
-      type: types.IMAGE_OR_TEXT,
-      x: 299, y: 95, w: 586, h: 515, a: 0,
-      background: "white",
-      text: {
-        valign: 'flex-start',
-        halign: 'center',
-        color: 'black',
-        shadow: 'white',
-        strokeSize: '0px',
-        strokeColor: 'rgba(255,255,255,0.01)',
-        value: ''
-      },
-      image : {
-        align: 'center center'
-      },
-      css : `
+    frames: [
+      {
+        type: types.IMAGE_OR_TEXT,
+        x: 299,
+        y: 95,
+        w: 586,
+        h: 515,
+        a: 0,
+        background: "white",
+        text: {
+          valign: "flex-start",
+          halign: "center",
+          color: "black",
+          shadow: "white",
+          strokeSize: "0px",
+          strokeColor: "rgba(255,255,255,0.01)",
+          value: "",
+        },
+        image: {
+          align: "center center",
+        },
+        css: `
 transform: matrix3d(-0.224997, 0.180977, 0, -0.000562, 
 -0.683572, 0.025673, 0, 0.000058, 
 0, 0, 1, 0, 
 437.746929, 237.838708, 0, 1);
 transform-origin: 3px 75px 0px;
-`
-    },
-    {
-      type: types.IMAGE,
-      x: 0, y: 0, w: 853, h: 844, a: 0,
-      image: {
-        url: 'images/trump_foreground.png',
-        align: 'center center'
-      }
-    }]
+`,
+      },
+      {
+        type: types.IMAGE,
+        x: 0,
+        y: 0,
+        w: 853,
+        h: 844,
+        a: 0,
+        image: {
+          url: "images/trump_foreground.png",
+          align: "center center",
+        },
+      },
+    ],
   },
   {
-    keyword: 'musk',
-    template: 'default.html',
-    
+    keyword: "musk",
+    template: "default.html",
+
     meme: {
-      w: 1578 ,
-      h: 754 ,
+      w: 1578,
+      h: 754,
       image: {
-        url: 'images/musk_background.png',
-        align: 'center center'
-      }
+        url: "images/musk_background.png",
+        align: "center center",
+      },
     },
     defaultFrameIndex: 0,
-    frames: [{
-      type: types.IMAGE_OR_TEXT,
-      x: 20, y: 20, w: 1211, h: 714, a: 0,
-      text: {
-        valign: 'flex-start',
-        halign: 'flex-start',
-        color: 'white',
-        shadow: 'white',
-        css: `
+    frames: [
+      {
+        type: types.IMAGE_OR_TEXT,
+        x: 20,
+        y: 20,
+        w: 1211,
+        h: 714,
+        a: 0,
+        text: {
+          valign: "flex-start",
+          halign: "flex-start",
+          color: "white",
+          shadow: "white",
+          css: `
           position: relative;
           width: 1011px;
         `,
-        value: ''
+          value: "",
+        },
+        image: {
+          align: "left center",
+        },
       },
-      image : {
-        align: 'left center'
+      {
+        type: types.IMAGE,
+        x: 0,
+        y: 0,
+        w: 1578,
+        h: 754,
+        a: 0,
+        image: {
+          url: "images/musk_foreground.png",
+          align: "center right",
+        },
       },
-    },
-    {
-      type: types.IMAGE,
-      x: 0, y: 0, w: 1578, h: 754, a: 0,
-      image: {
-        url: 'images/musk_foreground.png',
-        align: 'center right'
-      }
-    }]
+    ],
   },
   {
-    keyword: 'bobross',
-    template: 'default.html',
-    
+    keyword: "bobross",
+    template: "default.html",
+
     meme: {
-      w: 1379 ,
-      h: 900 ,
+      w: 1379,
+      h: 900,
       image: {
-        url: 'images/bob_background.png',
-        align: 'center center'
-      }
+        url: "images/bob_background.png",
+        align: "center center",
+      },
     },
     defaultFrameIndex: 0,
-    frames: [{
-      type: types.IMAGE_OR_TEXT,
-      x: 0, y: 0, w: 586, h: 647, a: 0,
-      background: "white",
-      text: {
-        valign: 'flex-start',
-        halign: 'center',
-        color: 'black',
-        shadow: 'black',
-        strokeSize: '0px',
-        strokeColor: 'rgba(255,255,255,0)',
-        value: ''
-      },
-      image : {
-        align: 'center center'
-      },
-      css : `
+    frames: [
+      {
+        type: types.IMAGE_OR_TEXT,
+        x: 0,
+        y: 0,
+        w: 586,
+        h: 647,
+        a: 0,
+        background: "white",
+        text: {
+          valign: "flex-start",
+          halign: "center",
+          color: "black",
+          shadow: "black",
+          strokeSize: "0px",
+          strokeColor: "rgba(255,255,255,0)",
+          value: "",
+        },
+        image: {
+          align: "center center",
+        },
+        css: `
       transform: matrix3d(0.275028, -0.198761, 0, -0.000363, 
         0.028931, 0.849221, 0, 0.000028, 
         0, 0, 1, 0, 
         876, 159, 0, 1);
 transform-origin: 0px 0px 0px;
-`
-    },
-    {
-      type: types.IMAGE,
-      x: 0, y: 0, w: 1379, h: 900, a: 0,
-      image: {
-        url: 'images/bob_foreground.png',
-        align: 'center center'
-      }
-    }]
-  }
+`,
+      },
+      {
+        type: types.IMAGE,
+        x: 0,
+        y: 0,
+        w: 1379,
+        h: 900,
+        a: 0,
+        image: {
+          url: "images/bob_foreground.png",
+          align: "center center",
+        },
+      },
+    ],
+  },
 ];
 
 module.exports = {
-  memeConfig: () => memes,
-  types
-}
+  memes,
+  types,
+};
--- bot/index.js
+++ bot/index.js
@@ -1,142 +1,159 @@
 /*jshint esversion: 9 */
-let { Client, MessageAttachment } = require('discord.js');
-  auth = require('./config/auth.json'),
-  { memeConfig, type } = require('./config/memes.js'),
-  
-  textToImage = require('text-to-image'),
-  fs = require('fs'),
-  layoutBuffer = require('./layout'),
-  {  parse, isGraph, extract } = require('./makeNumbers');
+const { Client, MessageAttachment } = require("discord.js");
+const auth = require("./config/auth.json");
+const { memes, type } = require("./config/memes.js");
 
-var char = auth.char || '!';
+const textToImage = require("text-to-image");
+const fs = require("fs");
+const layoutBuffer = require("./layout");
+const { parse, isGraph, extract } = require("./makeNumbers");
 
+var char = auth.char || "!";
 
 var client = new Client();
 
-// when messages come in
-client.on('message', async message => {
-  try {
-    
-    let memes = memeConfig();
-    memes.forEach(async function(config){
-      
-      config = JSON.parse(JSON.stringify(config));
-      
-      //figure out if it's a command we want :p
-      let isCommand = message.cleanContent.startsWith(char + config.keyword);
+const memeMap = {};
 
-      if (!isCommand) {
+for (const meme of memes) {
+  const config = JSON.stringify(meme);
+  memeMap[meme.keyword] = () => JSON.parse(config);
+}
+
+// when messages come in
+client.on("message", async (message) => {
+  try {
+    //figure out if it's a command we want :p command is always going to be the first word
+    let isCommand = message.cleanContent.startsWith(char);
+    //does it looks like a command we care about
+    if (!isCommand) {
+      return;
+    }
+    console.log("it's a command");
+
+    const keyword = message.cleanContent.replace(char, "").split(/\s+/)[0];
+    const commandShouldBe = char + keyword;
+
+    let config = memeMap[keyword];
+
+    console.log(keyword);
+
+    //is it actually a config
+    if (!config) {
+      return;
+    }
+
+    console.log("We gotta a config!");
+
+    config = config();
+
+    message.channel.startTyping(1);
+
+    //param parsing regex
+    let paramRegex = /\b[a-z_]+=[^\n=]+(\n|$)/gi;
+
+    let matches = message.cleanContent.match(paramRegex);
+    //return KV array into as object
+    config.params = (matches || []).reduce((a, b) => {
+      let bits = b.trim().split("=");
+      return { [bits[0]]: bits[1], ...a };
+    }, {});
+
+    //trim off params
+    let text = message.cleanContent.trim().replace(paramRegex, "");
+
+    //trim off command
+    text = text.replace(commandShouldBe, "").trim();
+
+    if (!config.frames) {
+      config.frames = [];
+    }
+
+    //custom parser for the templates to use, can be defined in the config
+    if (config.custom) {
+      for (let [key, func] of Object.entries(config.custom)) {
+        config.custom[key] = func(text, config);
+      }
+    }
+
+    if (isGraph(text)) {
+      let extracted = extract(text);
+
+      let frame = config.frames[config.defaultFrameIndex];
+
+      frame.plot = {
+        extracted,
+        data: parse(extracted.chars, frame.w, frame.h),
+      };
+    }
+
+    //if extra text is used in the command set that text so it can be rendered
+    else if (text) {
+      //the config allows for multiple layers
+      //config.defaultFrameIndex allows you to define which
+      //layer is desired for discord to render text or images into
+      if (config.frames.length) {
+        config.frames[config.defaultFrameIndex].text.value = text;
+      }
+    }
+
+    //if no extra text find an image in the last 60 messages and attatch that instead
+    else if (!text) {
+      let msgs = await message.channel.messages.fetch({ limit: 60 });
+      let msg = msgs.find((m) => {
+        return !m.author.bot && m.attachments.size;
+      });
+
+      //no attachments found in the last 60
+      if (!msg) {
+        message.channel.send(`Opps, try pasting an image into discord.`);
         return;
       }
 
-      message.channel.startTyping(1);
-
-      //param parsing regex
-      let paramRegex = /\b[a-z_]+=[^\n=]+(\n|$)/ig;
-
-      let matches = message.cleanContent.match(paramRegex);
-      //return KV array into as object 
-      config.params = ( matches || [] ).reduce((a,b) => {
-          let bits = b.trim().split('=');
-          return { [bits[0]]:bits[1],  ...a };
-      }, {});
-      
-      //trim off params
-      let text = message.cleanContent.trim().replace(paramRegex,'');
-      
-      //trim off command
-      text = text.replace(char + config.keyword, '').trim();
-
-      if(!config.frames) {
-        config.frames = [];
-      }
-
-      //custom parser for the templates to use, can be defined in the config 
-      if(config.custom) {
-        for (let [key, func] of Object.entries(config.custom)) {
-          config.custom[key] = func(text, config);
+      if (config.frames.length) {
+        if (!config.frames[config.defaultFrameIndex].image) {
+          config.frames[config.defaultFrameIndex].image = {};
         }
+
+        config.frames[
+          config.defaultFrameIndex
+        ].image.url = msg.attachments.first().url;
       }
+    }
 
-      if(isGraph(text)) {
-        let extracted = extract(text);
+    //load puppeteer chrome headless, load and hydrate template and return as image buffer
+    let buffer = await layoutBuffer(config, auth.output);
 
-        let frame = config.frames[config.defaultFrameIndex];
-        
-        frame.plot = {
-          extracted,
-          data: parse(extracted.chars, frame.w, frame.h)
-        };
-      } else
+    //djs stuff
+    const attachment = new MessageAttachment(buffer, "image.png");
+    message.channel.send(``, attachment);
 
-      //if extra text is used in the command set that text so it can be rendered
-      if(text) {
-        //the config allows for multiple layers 
-        //config.defaultFrameIndex allows you to define which 
-        //layer is desired for discord to render text or images into
-        if(config.frames.length) {
-          config.frames[config.defaultFrameIndex].text.value = text;
-        }
-      }
-      
-      //if no extra text find an image in the last 60 messages and attatch that instead
-      else if(!text) {
-        let msgs = await message.channel.messages.fetch({ limit: 60 });
-        let msg = msgs.find(m => { 
-          return !m.author.bot && m.attachments.size
-        });
-        
-        //no attachments found in the last 60
-        if(!msg) {
-          message.channel.send(`Opps, try pasting an image into discord.`);
-          return;
-        }
-        
-        if(config.frames.length) {
-          if(!config.frames[config.defaultFrameIndex].image) {
-            config.frames[config.defaultFrameIndex].image = {};
-          }
-
-          config.frames[config.defaultFrameIndex].image.url = msg.attachments.first().url
-        }
-      }
-
-      //load puppeteer chrome headless, load and hydrate template and return as image buffer
-      let buffer = await layoutBuffer(config, auth.output);
-      
-      //djs stuff
-      const attachment = new MessageAttachment(buffer, 'image.png');
-      message.channel.send(``, attachment);
-
-    }); 
-
-  } catch(ex) { console.log(ex) }
+    // });
+  } catch (ex) {
+    console.log(ex);
+  }
 
   message.channel.stopTyping(true);
-
 });
-
-
-
 
 // client.on('ready', async => {
 //   client.guilds.forEach(server => { console.log(server.name); });
 // });
 
 // capture a whole pile of useful information
-client.on('error',            console.log);
-client.on('guildUnavailable', guild    => console.log('guild unavailable:', guild.id));
-client.on('rateLimit',        info     => console.log('rate limited', info));
-client.on('reconnecting',     ()       => console.log('reconnecting'));
-client.on('resume',           replayed => console.log('resume: ', replayed));
-client.on('warn',             info     => console.log('warn:', info));
+client.on("error", console.log);
+client.on("guildUnavailable", (guild) =>
+  console.log("guild unavailable:", guild.id)
+);
+client.on("rateLimit", (info) => console.log("rate limited", info));
+client.on("reconnecting", () => console.log("reconnecting"));
+client.on("resume", (replayed) => console.log("resume: ", replayed));
+client.on("warn", (info) => console.log("warn:", info));
 
-client.on('disconnect',       info     => console.log('disconnect:', info));
+client.on("disconnect", (info) => console.log("disconnect:", info));
 
 // something goes wrong we didnt think of or having got around to putting a band-aid fix on
-process.on('uncaughtException', err => console.log('uncaughtException: ', err));
+process.on("uncaughtException", (err) =>
+  console.log("uncaughtException: ", err)
+);
 
 client.login(auth.token);
-
-
-
